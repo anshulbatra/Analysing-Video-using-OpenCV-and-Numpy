0197d8950e4799df4c2b05dd41f99ff80de2f44d
Anshul Batra <anshul_batra99@ymail.com>
2020-08-05 11:05:55 +0530

    fetching frames and displaying them

---
+++ 01_retrieve-and-display.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 07:21:07 2020
+
+@author: anshul
+"""
+
+import cv2
+
+VFILE = "./Pexels Videos 2880.mp4"
+
+def get_frames(filename):
+    video = cv2.VideoCapture(filename)   
+    
+    while video.isOpened():
+        ret, frame = video.read()
+        
+        if ret:
+            yield frame
+        else:
+            yield None
+    yield None
+    
+    
+#Calling
+for frame in get_frames(VFILE):
+    if frame is None:
+        break
+    if cv2.waitKey(10) == 27:
+        break
+    
+    cv2.imshow('frame', frame)
+    
+cv2.destroyAllWindows()
+        
